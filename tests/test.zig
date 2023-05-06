ca1956ae56033982aec5096fbece6e90246eac2d
Paul Blischak <paul.blischak@gmail.com>
2023-05-05 21:10:28 -0400

    updating tests with comptime parameter specifications

--- tests/test.zig
+++ tests/test.zig
@@ -3,54 +3,89 @@ const zprob = @import("zprob");
 const Random = std.rand.Random;
 const DefaultPrng = std.rand.Xoshiro256;
 
-const TEST_REPS: usize = 10_000;
-const TEST_SEED: u64 = @as(u64, std.time.microTimestamp());
+const TestingState = struct {
+    reps: usize,
+    seed: u64,
+    tolerance: f64,
+
+    pub fn init(reps: ?usize, seed: ?u64, tol: ?f64) TestingState {
+        return .{
+            .reps = reps orelse 10_000,
+            .seed = seed orelse @intCast(u64, std.time.microTimestamp()),
+            .tol = tol orelse 0.1,
+        };
+    }
+};
+
+// Bernoulli Tests
 
 test "Bernoulli" {
-    var prng = DefaultPrng.init(TEST_SEED);
+    const ts = TestingState.init(null, null, null);
+    var prng = DefaultPrng.init(ts.seed);
     var rng = prng.random();
     var sum: f64 = 0.0;
-    for (0..TEST_REPS) |_| {
+    for (0..ts.reps) |_| {
         const samp = zprob.bernoulliSample(0.4, &rng);
         sum += @intToFloat(f64, samp);
     }
     const avg: f64 = sum / 10000.0;
     // zig fmt: off
     try std.testing.expectApproxEqAbs(
-        @as(f64, 0.4), avg, 0.1
+        @as(f64, 0.4), avg, ts.tolerance
     );
     // zig fmt: on
 }
 
 test "Binomial" {
-    var prng = DefaultPrng.init(TEST_SEED);
+    const ts = TestingState.init(null, null, null);
+    var prng = DefaultPrng.init(ts.seed);
     var rng = prng.random();
     var sum: f64 = 0.0;
-    for (0..TEST_REPS) |_| {
+    for (0..ts.reps) |_| {
         const samp = zprob.binomialSample(10, 0.2, &rng);
         sum += @intToFloat(f64, samp);
     }
     const avg: f64 = sum / 10000.0;
     // zig fmt: off
     try std.testing.expectApproxEqAbs(
-        @as(f64, 2.0), avg, 0.1
+        @as(f64, 2.0), avg, ts.tolerance
     );
     // zig fmt: on
 }
 
 test "Exponential" {
-    var prng = DefaultPrng.init(TEST_SEED);
+    const ts = TestingState.init(null, null, null);
+    var prng = DefaultPrng.init(ts.seed);
     var rng = prng.random();
     const lambda: f64 = 500.0;
     var sum: f64 = 0.0;
-    for (0..TEST_REPS) |_| {
-        const samp = zprob.exponentialSample(lambda, &rng);
+    var samp: f64 = undefined;
+    for (0..ts.reps) |_| {
+        samp = zprob.exponentialSample(f64, lambda, &rng);
         sum += samp;
     }
-    const avg: f64 = sum / 10000.0;
+    const avg: f64 = sum / @intToFloat(f64, ts.reps);
     // zig fmt: off
     try std.testing.expectApproxEqAbs(
-        1.0 / lambda, avg, 0.1
+        1.0 / lambda, avg, ts.tolerance
     );
     // zig fmt: on
+}
+
+// Normal Distribution Tests
+
+test "Normal Sample f64" {
+    const ts = TestingState.init(null, null, null);
+    var prng = DefaultPrng.init(ts.seed);
+    var rng = prng.random();
+    const mu: f64 = 5.0;
+    const sigma = 2.0;
+    var sum: f64 = 0.0;
+    var samp: f64 = undefined;
+    for (0..ts.reps) |_| {
+        samp = zprob.normalSample(f64, mu, sigma, &rng);
+        sum += samp;
+    }
+    const avg: f64 = sum / @intToFloat(f64, ts.reps);
+    try std.testing.expectApproxEqAbs(mu, avg, ts.tolerance);
 }
