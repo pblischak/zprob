1911514ff1225835e3be8cd1abfdb2667f3a86f3
Paul Blischak <paul.blischak@gmail.com>
2023-05-05 21:09:45 -0400

    adding comptime logic to exponential distr

--- src/exponential.zig
+++ src/exponential.zig
@@ -4,20 +4,40 @@ const std = @import("std");
 const Random = std.rand.Random;
 const DefaultPrng = std.rand.Xoshiro256;
 
-pub fn exponentialSample(lambda: f64, rng: *Random) f64 {
-    return -@log(1.0 - rng.float(f64)) / lambda;
+pub fn exponentialSample(comptime T: type, lambda: T, rng: *Random) T {
+    const l_f64 = @floatCast(f64, lambda);
+    const value = -@log(1.0 - rng.float(f64)) / l_f64;
+    switch (T) {
+        f64 => return value,
+        f32 => return @floatCast(f32, value),
+        else => @compileError("unknown floating point type"),
+    }
 }
 
-pub fn exponentialPdf(lambda: f64, x: f64) f64 {
+pub fn exponentialPdf(comptime T: type, lambda: T, x: T) T {
     if (x < 0) {
         return 0.0;
     }
-    return lambda * @exp(-lambda * x);
+    const l_f64 = @floatCast(f64, lambda);
+    const value = l_f64 * @exp(-l_f64 * x);
+
+    switch (T) {
+        f64 => return value,
+        f32 => return @floatCast(f32, value),
+        else => @compileError("unknown floating point type"),
+    }
 }
 
-pub fn exponentialLnPdf(lambda: f64, x: f64) f64 {
+pub fn exponentialLnPdf(comptime T: type, lambda: T, x: T) T {
     if (x < 0) {
         @panic("Cannot evaluate x less than 0.");
     }
-    return -lambda * x * @log(lambda) + 1.0;
+    const l_f64 = @floatCast(f64, lambda);
+    const value = -l_f64 * x * @log(l_f64) + 1.0;
+
+    switch (T) {
+        f64 => return value,
+        f32 => return @floatCast(f32, value),
+        else => @compileError("unknown floating point type"),
+    }
 }
