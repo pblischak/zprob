e22035c7759ba2c4e475c0c5c5b25297dd5553f9
Paul Blischak <paul.blischak@gmail.com>
2023-05-23 21:31:32 -0400

    adding simple test for dirichlet mean

--- src/dirichlet.zig
+++ src/dirichlet.zig
@@ -27,7 +27,7 @@ pub fn dirichletSample(
         x.* /= sum;
     }
 
-    return &x_vec;
+    return x_vec[0..];
 }
 
 pub fn dirichletPdf(comptime F: type, x_vec: []F, alpha_vec: []F) F {
@@ -58,4 +58,25 @@ fn lnMultivariateBeta(comptime F: type, alpha_vec: []F) F {
 
 fn multivariateBeta(comptime F: type, alpha_vec: []F) F {
     return @exp(lnMultivariateBeta(F, alpha_vec));
+}
+
+test "Dirichlet API" {
+    const seed = @intCast(u64, std.time.microTimestamp());
+    var prng = DefaultPrng.init(seed);
+    var rng = prng.random();
+    var alpha_vec = [3]f64{ 0.1, 0.1, 0.1 };
+    // const alpha_sum = 0.3;
+    var tmp: []f64 = undefined;
+    var res: [3]f64 = [3]f64{ 0.0, 0.0, 0.0 };
+    for (0..10_000) |_| {
+        tmp = try dirichletSample(f64, alpha_vec[0..], &rng, test_allocator);
+        defer test_allocator.free(tmp);
+        res[0] += tmp[0];
+        res[1] += tmp[1];
+        res[2] += tmp[2];
+    }
+    res[0] /= 10_000.0;
+    res[1] /= 10_000.0;
+    res[2] /= 10_000.0;
+    std.debug.print("\n{any}\n", .{res});
 }
