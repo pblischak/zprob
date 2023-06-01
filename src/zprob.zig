72fcf208246c1d3d6745cc3d95b702f76d112159
Paul Blischak <paul.blischak@gmail.com>
2023-05-31 22:48:52 -0400

    adding remaining distributions to main library file

--- src/zprob.zig
+++ src/zprob.zig
@@ -3,11 +3,18 @@
 /// Discrete Probability Distributions
 pub usingnamespace @import("bernoulli.zig");
 pub usingnamespace @import("binomial.zig");
+pub usingnamespace @import("geometric.zig");
+pub usingnamespace @import("multinomial.zig");
+pub usingnamespace @import("negative_binomial.zig");
+pub usingnamespace @import("poisson.zig");
 
 /// Continuous Probability Distributions
 pub usingnamespace @import("beta.zig");
+pub usingnamespace @import("chi_squared.zig");
+pub usingnamespace @import("dirichlet.zig");
 pub usingnamespace @import("exponential.zig");
 pub usingnamespace @import("gamma.zig");
+pub usingnamespace @import("multivariate_normal.zig");
 pub usingnamespace @import("normal.zig");
 
 /// Special Functions
