672de928dbfc3524e630af44b95510c48105fb67
Paul Blischak <paul.blischak@gmail.com>
2024-05-10 13:25:53 -0400

    [fix,test] bug fixes and tests added

--- src/bernoulli.zig
+++ src/bernoulli.zig
@@ -1,6 +1,10 @@
+//! Bernoulli distribution
+//!
+//! [https://en.wikipedia.org/wiki/Bernoulli_distribution](https://en.wikipedia.org/wiki/Bernoulli_distribution)
+
 const std = @import("std");
 const Allocator = std.mem.Allocator;
-const Random = std.rand.Random;
+const Random = std.Random;
 const utils = @import("utils.zig");
 
 /// Bernoulli distribution with parameter `p`.
@@ -22,8 +26,8 @@ pub fn Bernoulli(comptime I: type, comptime F: type) type {
             if (p < 0.0 or p > 1.0) {
                 @panic("Parameter `p` must be within the range 0 < p < 1.");
             }
-            // const random_val = self.prng.float(F);
-            const random_val = self.rand.float(F);
+            // Random floats can only be generated for f32 and f64
+            const random_val: F = @floatCast(self.rand.float(f64));
             if (p < random_val) {
                 return 0;
             } else {
@@ -39,6 +43,9 @@ pub fn Bernoulli(comptime I: type, comptime F: type) type {
             p: F,
             allocator: Allocator,
         ) ![]I {
+            if (p < 0.0 or p > 1.0) {
+                @panic("Parameter `p` must be within the range 0 < p < 1.");
+            }
             var res = try allocator.alloc(I, size);
             for (0..size) |i| {
                 res[i] = self.sample(p);
@@ -49,8 +56,8 @@ pub fn Bernoulli(comptime I: type, comptime F: type) type {
 }
 
 test "Sample Bernoulli" {
-    var seed: u64 = @intCast(std.time.microTimestamp());
-    var prng = std.rand.Xoroshiro128.init(seed);
+    const seed: u64 = @intCast(std.time.microTimestamp());
+    var prng = std.Random.DefaultPrng.init(seed);
     var rand = prng.random();
     var bernoulli = Bernoulli(u8, f64).init(&rand);
     const val = bernoulli.sample(0.4);
@@ -58,8 +65,8 @@ test "Sample Bernoulli" {
 }
 
 test "Sample Bernoulli Slice" {
-    var seed: u64 = @intCast(std.time.microTimestamp());
-    var prng = std.rand.Xoroshiro128.init(seed);
+    const seed: u64 = @intCast(std.time.microTimestamp());
+    var prng = std.Random.DefaultPrng.init(seed);
     var rand = prng.random();
     var bernoulli = Bernoulli(u8, f64).init(&rand);
 
@@ -70,21 +77,43 @@ test "Sample Bernoulli Slice" {
 }
 
 test "Bernoulli Mean" {
-    var seed: u64 = @intCast(std.time.microTimestamp());
-    var prng = std.rand.Xoroshiro128.init(seed);
+    const seed: u64 = @intCast(std.time.microTimestamp());
+    var prng = std.Random.DefaultPrng.init(seed);
     var rand = prng.random();
     var bernoulli = Bernoulli(u8, f64).init(&rand);
 
-    const p: f64 = 0.2;
-    var samp: u8 = undefined;
-    var sum: f64 = 0.0;
-    for (0..10_000) |_| {
-        samp = bernoulli.sample(p);
-        sum += @as(f64, @floatFromInt(samp));
+    const p_vec = [_]f64{ 0.05, 0.1, 0.2, 0.4, 0.5, 0.6, 0.8, 0.9, 0.95 };
+
+    std.debug.print("\n", .{});
+    for (p_vec) |p| {
+        var samp: u8 = undefined;
+        var sum: f64 = 0.0;
+        for (0..10_000) |_| {
+            samp = bernoulli.sample(p);
+            sum += @as(f64, @floatFromInt(samp));
+        }
+        const mean: f64 = p;
+        const avg: f64 = sum / 10_000.0;
+        const variance: f64 = p * (1.0 - p);
+        std.debug.print("Mean: {}\tAvg: {}\tStdDev {}\n", .{ mean, avg, @sqrt(variance) });
+        try std.testing.expectApproxEqAbs(mean, avg, @sqrt(variance));
     }
-    const mean: f64 = p;
-    const avg: f64 = sum / 10_000.0;
-    const variance: f64 = p * (1.0 - p);
-    std.debug.print("Mean: {}\tAvg: {}\tVariance {}\n", .{ mean, avg, variance });
-    try std.testing.expectApproxEqAbs(mean, avg, variance);
+}
+
+test "Bernoulli with Different Types" {
+    const seed: u64 = @intCast(std.time.microTimestamp());
+    var prng = std.Random.DefaultPrng.init(seed);
+    var rand = prng.random();
+
+    const int_types = [_]type{ u8, u16, u32, u64, u128, i8, i16, i32, i64, i128 };
+    const float_types = [_]type{ f16, f32, f64, f128 };
+
+    std.debug.print("\n", .{});
+    inline for (int_types) |i| {
+        inline for (float_types) |f| {
+            var bernoulli = Bernoulli(i, f).init(&rand);
+            const val = bernoulli.sample(0.25);
+            std.debug.print("Bernoulli({any}, {any}):\t{}\n", .{ i, f, val });
+        }
+    }
 }
