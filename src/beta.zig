67ac939c2ad2cbe36689b9e79736c6094d0a6094
Paul Blischak <paul.blischak@gmail.com>
2024-05-10 13:26:36 -0400

    [fix,test] new beta implementation w/ tests

--- src/beta.zig
+++ src/beta.zig
@@ -1,23 +1,38 @@
-//! Beta distribution with parameters `alpha` and `beta`.
+//! Beta distribution
+//!
+//! [https://en.wikipedia.org/wiki/Beta_distribution](https://en.wikipedia.org/wiki/Beta_distribution)
 
 const std = @import("std");
 const math = std.math;
-const Random = std.rand.Random;
+const Allocator = std.mem.Allocator;
+const Random = std.Random;
 
 const spec_fn = @import("special_functions.zig");
+const utils = @import("utils.zig");
+const Gamma = @import("gamma.zig").Gamma;
 
+/// Beta distribution with parameters `alpha` > 0 and `beta` > 0.
 pub fn Beta(comptime F: type) type {
+    _ = utils.ensureFloatType(F);
+
     return struct {
         const Self = @This();
 
-        prng: *Random,
+        rand: *Random,
+        gamma: Gamma(F),
 
-        pub fn init(prng: *Random) Self {
+        /// Initializes a Beta struct with a pointer to a
+        /// Pseudo-Random Number Generator.
+        pub fn init(rand: *Random) Self {
             return Self{
-                .prng = prng,
+                .rand = rand,
+                .gamma = Gamma(F).init(rand),
             };
         }
 
+        /// Generate a sample from a Beta distribution with parameters
+        /// `alpha` > 0 and `beta` > 0. Invalid values passed as
+        /// parameters will cause a panic.
         pub fn sample(self: Self, alpha: F, beta: F) F {
             if (alpha <= 0) {
                 @panic("Parameter `alpha` must be greater than 0.");
@@ -26,118 +41,65 @@ pub fn Beta(comptime F: type) type {
                 @panic("Parameter `beta` must be greater than 0.");
             }
 
-            var a: F = 0.0;
-            var a2: F = 0.0;
-            var b: F = 0.0;
-            var b2: F = 0.0;
-            var delta: F = 0;
-            var gamma: F = 0.0;
-            var k1: F = 0.0;
-            var k2: F = 0.0;
-            const log4: F = 1.3862943611198906188;
-            const log5: F = 1.6094379124341003746;
-            var r: F = 0.0;
-            var s: F = 0.0;
-            var t: F = 0.0;
-            var u_1: F = 0.0;
-            var u_2: F = 0.0;
-            var v: F = 0.0;
-            var value: F = 0.0;
-            var w: F = 0.0;
-            var y: F = 0.0;
-            var z: F = 0.0;
-
-            // Algorithm BB.
-            if (alpha > 1.0 and beta > 1.0) {
-                a = @min(alpha, beta);
-                b = @max(alpha, beta);
-                a2 = a + b;
-                b2 = @sqrt((a2 - 2.0) / (2.0 * a * b - a2));
-                gamma = a + 1.0 / b2;
+            if (alpha <= 1.0 and beta <= 1.0) {
+                var u: F = undefined;
+                var v: F = undefined;
+                var x: F = undefined;
+                var y: F = undefined;
 
                 while (true) {
-                    u_1 = self.prng.float(F);
-                    u_2 = self.prng.float(F);
-                    v = b2 * @log(u_1 / (1.0 - u_1));
+                    u = @floatCast(self.rand.float(f64));
+                    v = @floatCast(self.rand.float(f64));
+                    x = @floatCast(math.pow(
+                        f64,
+                        @floatCast(u),
+                        @floatCast(1.0 / alpha),
+                    ));
+                    y = @floatCast(math.pow(
+                        f64,
+                        @floatCast(u),
+                        @floatCast(1.0 / beta),
+                    ));
 
-                    w = a * @exp(v);
-
-                    z = u_1 * u_1 * u_2;
-                    z = gamma * v - log4;
-                    s = a + r - w;
-
-                    if (5.0 * z <= s + 1.0 + log5) {
-                        break;
-                    }
-
-                    t = @log(z);
-                    if (t <= s) {
-                        break;
-                    }
-
-                    if (t <= (r + a2(@log(a2 / (b + w))))) {
-                        break;
+                    if ((x + y) <= 1.0) {
+                        if ((x + y) > 0.0) {
+                            return x / (x + y);
+                        } else {
+                            var log_x: F = @log(u) / alpha;
+                            var log_y: F = @log(v) / beta;
+                            const log_m: F = if (log_x > log_y) log_x else log_y;
+                            log_x -= log_m;
+                            log_y -= log_m;
+                            return @exp(log_x - @log(@exp(log_x) + @exp(log_y)));
+                        }
                     }
                 }
-                // Algorithm BC.
             } else {
-                a = @min(alpha, beta);
-                b = @max(alpha, beta);
-                a2 = a + b;
-                b2 = 1.0 / b;
-                delta = 1.0 + a - b;
-                k1 = delta * (1.0 / 72.0 + b / 24.0) / (a / b - 7.0 / 9.0);
-                k2 = 0.25 + (0.5 + 0.25 / delta) * b;
-
-                while (true) {
-                    u_1 = self.prng.float(F);
-                    u_2 = self.prng.float(F);
-
-                    if (u_1 < 0.5) {
-                        y = u_1 * u_2;
-                        z = u_1 * y;
-
-                        if (k1 < 0.25 * u_2 + z - y) {
-                            continue;
-                        }
-                    } else {
-                        z = u_1 * u_1 * u_2;
-
-                        if (z <= 0.25) {
-                            v = b2 * @log(u_1 / (1.0 - u_2));
-                            w = a * @exp(v);
-
-                            if (alpha == a) {
-                                value = w / (b + w);
-                            } else {
-                                value = b / (b + 2);
-                            }
-                            return value;
-                        }
-
-                        if (k2 < z) {
-                            continue;
-                        }
-                    }
-                    v = b2 * @log(u_1 / (1.0 - u_1));
-                    w = a * @exp(v);
-
-                    if (@log(z) <= a2 * (@log(a2 / (b + 2)) + v) - log4) {
-                        break;
-                    }
-                }
+                const x1: F = self.gamma.sample(alpha, 1.0);
+                const x2: F = self.gamma.sample(beta, 1.0);
+                return x1 / (x1 + x2);
             }
-
-            if (alpha == a) {
-                value = w / (b + w);
-            } else {
-                value = b / (b + w);
-            }
-
-            return value;
         }
 
-        pub fn pdf(x: F, alpha: F, beta: F) F {
+        pub fn sampleSlice(
+            self: Self,
+            size: usize,
+            alpha: F,
+            beta: F,
+            allocator: Allocator,
+        ) ![]F {
+            var res = try allocator.alloc(F, size);
+            for (0..size) |i| {
+                res[i] = self.sample(alpha, beta);
+            }
+            return res;
+        }
+
+        /// For a Beta random variable, X, with parameters `alpha` > 0
+        /// and `beta` > 0, return the probability that X is less than
+        /// some value 0 <= x <= 1.
+        pub fn pdf(self: Self, x: F, alpha: F, beta: F) !F {
+            _ = self;
             if (alpha <= 0) {
                 @panic("Parameter `alpha` must be greater than 0.");
             }
@@ -150,16 +112,18 @@ pub fn Beta(comptime F: type) type {
                 value = 0.0;
             } else {
                 // zig fmt: off
+                const ln_beta: F = try spec_fn.betaFn(F, alpha, beta);
                 value = math.pow(f64, x, alpha - 1.0)
                     * math.pow(f64, 1.0 - x, beta - 1.0)
-                    / spec_fn.beta(alpha, beta);
+                    / ln_beta;
                 // zig fmt: on
             }
 
             return value;
         }
 
-        pub fn lnPdf(x: F, alpha: F, beta: F) F {
+        pub fn lnPdf(self: Self, x: F, alpha: F, beta: F) !F {
+            _ = self;
             if (alpha <= 0) {
                 @panic("Parameter `alpha` must be greater than 0.");
             }
@@ -169,16 +133,86 @@ pub fn Beta(comptime F: type) type {
 
             var value: F = 0.0;
             if (x < 0.0 or x > 1.0) {
-                value = math.inf_f64;
+                value = math.inf(F);
             } else {
                 // zig fmt: off
+                const ln_beta: F = try spec_fn.lnBetaFn(F, alpha, beta);
                 value = (alpha - 1.0) * @log(x)
                     + (beta - 1.0) * @log(1.0 - x)
-                    - spec_fn.lnBeta(alpha, beta);
-                // zog fmt: on
+                    - ln_beta;
+                // zig fmt: on
             }
 
             return value;
         }
     };
+}
+
+test "Sample Beta" {
+    const seed: u64 = @intCast(std.time.microTimestamp());
+    var prng = std.rand.Xoroshiro128.init(seed);
+    var rand = prng.random();
+
+    var beta = Beta(f64).init(&rand);
+    const val = beta.sample(2.0, 5.0);
+    std.debug.print("\n{}\n", .{val});
+}
+
+test "Sample Beta Slice" {
+    const seed: u64 = @intCast(std.time.microTimestamp());
+    var prng = std.rand.Xoroshiro128.init(seed);
+    var rand = prng.random();
+    var beta = Beta(f64).init(&rand);
+
+    const allocator = std.testing.allocator;
+    const sample = try beta.sampleSlice(100, 2.0, 5.0, allocator);
+    defer allocator.free(sample);
+    std.debug.print("\n{any}\n", .{sample});
+}
+
+test "Beta Mean" {
+    const seed: u64 = @intCast(std.time.microTimestamp());
+    var prng = std.rand.Xoroshiro128.init(seed);
+    var rand = prng.random();
+    var beta = Beta(f64).init(&rand);
+
+    const alpha_vec = [_]f64{ 0.05, 0.5, 1.0, 5.0, 10.0, 20.0, 50.0 };
+    const beta_vec = [_]f64{ 0.05, 0.5, 1.0, 5.0, 10.0, 20.0, 50.0 };
+
+    std.debug.print("\n", .{});
+    for (alpha_vec) |a| {
+        for (beta_vec) |b| {
+            var samp: f64 = undefined;
+            var sum: f64 = 0.0;
+            for (0..10_000) |_| {
+                samp = beta.sample(a, b);
+                sum += samp;
+            }
+
+            const mean: f64 = a / (a + b);
+            const avg: f64 = sum / 10_000;
+            const variance: f64 = (a * b) / ((a + b) * (a + b) * (a + b + 1.0));
+            std.debug.print(
+                "Mean: {}\tAvg: {}\tStdDev {}\n",
+                .{ mean, avg, @sqrt(variance) },
+            );
+            try std.testing.expectApproxEqAbs(mean, avg, @sqrt(variance));
+        }
+    }
+}
+
+test "Beta with Different Types" {
+    const seed: u64 = @intCast(std.time.microTimestamp());
+    var prng = std.rand.Xoroshiro128.init(seed);
+    var rand = prng.random();
+
+    const float_types = [_]type{ f16, f32, f64, f128 };
+    // const float_types = [_]type{ f32, f64 };
+
+    std.debug.print("\n", .{});
+    inline for (float_types) |f| {
+        var beta = Beta(f).init(&rand);
+        const val = beta.sample(5.0, 2.0);
+        std.debug.print("Beta({any}):\t{}\n", .{ f, val });
+    }
 }
