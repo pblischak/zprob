23f95c1f9f1a64bc70accb1a3c57d6e215bc7878
Paul Blischak <paul.blischak@gmail.com>
2024-05-13 16:24:58 -0400

    [misc] scripts for builds/tests

---
+++ scripts/clean_build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+set -xe
+
+pushd $(dirname "${BASH_SOURCE[0]}")/..
+echo $(pwd)
+
+echo "Removing Zig cache and output..."
+rm -rf zig-cache zig-out
+
+echo "Building module..."
+zig build
+
+echo "Building docs..."
+zig build docs
+
+popd
