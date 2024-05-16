c880aa2ffd961e4d76baf4206a4922a5906314ab
Paul Blischak <paul.blischak@gmail.com>
2024-05-15 20:23:39 -0400

    [fix] removing support for f16

--- src/uniform.zig
+++ src/uniform.zig
@@ -138,7 +138,7 @@ test "Uniform with Different Types" {
     var prng = std.Random.DefaultPrng.init(seed);
     var rand = prng.random();
 
-    const float_types = [_]type{ f16, f32, f64, f128 };
+    const float_types = [_]type{ f32, f64, f128 };
 
     std.debug.print("\n", .{});
     inline for (float_types) |f| {
