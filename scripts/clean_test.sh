5392e70230057705b84fd474349f9094da503cb3
Paul Blischak <paul.blischak@gmail.com>
2024-06-16 17:46:36 -0400

    [fix] update scripts for 0.13 and new .zig-cache

--- scripts/clean_test.sh
+++ scripts/clean_test.sh
@@ -6,7 +6,7 @@ pushd $(dirname "${BASH_SOURCE[0]}")/..
 echo $(pwd)
 
 echo "Removing Zig cache and output..."
-rm -rf zig-cache zig-out 
+rm -rf .zig-cache zig-out 
 
 echo "Building and running module tests..."
 zig build test
