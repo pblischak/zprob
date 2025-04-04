724c5d265877c8010d1d561741552bd923a3e658
Paul Blischak <paul.blischak@gmail.com>
2025-04-04 17:45:54 -0400

    fix: updating distribution sampling example

---
+++ examples/distribution_sampling/README.md
@@ -0,0 +1,7 @@
+# Example: Sampling from a Distribution
+
+To build and run the project, type:
+
+```bash
+zig build run
+```
--- examples/distribution_sampling/src/main.zig
+++ examples/distribution_sampling/src/main.zig
@@ -6,7 +6,7 @@ const zprob = @import("zprob");
 pub fn main() !void {
     var prng = std.Random.DefaultPrng.init(blk: {
         var seed: u64 = undefined;
-        try std.os.getrandom(std.mem.asBytes(&seed));
+        try std.posix.getrandom(std.mem.asBytes(&seed));
         break :blk seed;
     });
     var rand = prng.random();
@@ -14,7 +14,7 @@ pub fn main() !void {
 
     var sample: i32 = 0;
     for (0..100) |_| {
-        sample = binomial.sample(50, 0.5);
+        sample = try binomial.sample(50, 0.5);
         std.debug.print("{}\n", .{sample});
     }
 }
