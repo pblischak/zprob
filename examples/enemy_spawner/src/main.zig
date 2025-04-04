151d71c71b1dde79c53490bbe9aa78911d5b5766
Paul Blischak <paul.blischak@gmail.com>
2025-04-04 17:46:13 -0400

    fix: updating enemy spawner example

--- examples/enemy_spawner/src/main.zig
+++ examples/enemy_spawner/src/main.zig
@@ -25,74 +25,73 @@ const MAX_HEIGHT: u32 = 180;
 // Weights for sampling enemy types.
 var ENEMY_TYPE_WEIGHTS = [_]f64{ 0.9, 0.1 };
 
-pub fn EnemySpawner() type {
-    return struct {
-        const Self = @This();
-        env: RandomEnvironment,
-        allocator: Allocator,
+const EnemySpawner = struct {
+    const Self = @This();
+    env: RandomEnvironment,
+    allocator: Allocator,
 
-        pub fn init(allocator: Allocator) !Self {
-            return Self{
-                .env = try RandomEnvironment.init(allocator),
-                .allocator = allocator,
-            };
-        }
+    pub fn init(allocator: Allocator) !Self {
+        return Self{
+            .env = try RandomEnvironment.init(allocator),
+            .allocator = allocator,
+        };
+    }
 
-        pub fn deinit(self: *Self) void {
-            self.env.deinit();
-        }
+    pub fn deinit(self: *Self) void {
+        self.env.deinit();
+    }
 
-        pub fn spawnEnemies(self: *Self, count: usize) ![]Enemy {
-            var enemies = try self.allocator.alloc(Enemy, count);
-            for (0..count) |i| {
-                enemies[i] = self.spawnEnemy();
-            }
-            return enemies;
+    pub fn spawnEnemies(self: *Self, count: usize) ![]Enemy {
+        var enemies = try self.allocator.alloc(Enemy, count);
+        for (0..count) |i| {
+            enemies[i] = try self.spawnEnemy();
         }
+        return enemies;
+    }
 
-        fn spawnEnemy(self: *Self) Enemy {
-            return switch (self.env.rWeightedSample(
-                EnemyType,
-                std.enums.values(EnemyType),
-                ENEMY_TYPE_WEIGHTS[0..],
-            )) {
-                .Regular => self.spawnRegularEnemy(),
-                .Berserker => self.spawnBerserkerEnemy(),
-            };
-        }
+    fn spawnEnemy(self: *Self) !Enemy {
+        const enemy_type = try self.env.rWeightedSample(
+            EnemyType,
+            std.enums.values(EnemyType),
+            ENEMY_TYPE_WEIGHTS[0..],
+        );
+        return switch (enemy_type) {
+            .Regular => self.spawnRegularEnemy(),
+            .Berserker => self.spawnBerserkerEnemy(),
+        };
+    }
 
-        fn spawnRegularEnemy(self: *Self) Enemy {
-            var uniform_uint = zprob.UniformInt(u8).init(self.env.getRand());
-            const max_health = uniform_uint.sample(10, 15);
-            return Enemy{
-                .enemy_type = .Regular,
-                .x = self.env.rUniformUInt(0, MAX_WIDTH - 1),
-                .y = self.env.rUniformUInt(0, MAX_HEIGHT - 1),
-                .max_health = max_health,
-                .current_health = max_health,
-                .attack = uniform_uint.sample(2, 6),
-                .defense = uniform_uint.sample(3, 5),
-            };
-        }
+    fn spawnRegularEnemy(self: *Self) Enemy {
+        var uniform_uint = zprob.UniformInt(u8).init(self.env.getRand());
+        const max_health = uniform_uint.sample(10, 15);
+        return Enemy{
+            .enemy_type = .Regular,
+            .x = self.env.rUniformUInt(0, MAX_WIDTH - 1),
+            .y = self.env.rUniformUInt(0, MAX_HEIGHT - 1),
+            .max_health = max_health,
+            .current_health = max_health,
+            .attack = uniform_uint.sample(2, 6),
+            .defense = uniform_uint.sample(3, 5),
+        };
+    }
 
-        fn spawnBerserkerEnemy(self: *Self) Enemy {
-            var uniform_uint = zprob.UniformInt(u8).init(self.env.getRand());
-            const max_health = uniform_uint.sample(25, 30);
-            return Enemy{
-                .enemy_type = .Berserker,
-                .x = self.env.rUniformUInt(0, MAX_WIDTH - 1),
-                .y = self.env.rUniformUInt(0, MAX_HEIGHT - 1),
-                .max_health = max_health,
-                .current_health = max_health,
-                .attack = uniform_uint.sample(14, 18),
-                .defense = uniform_uint.sample(11, 15),
-            };
-        }
-    };
-}
+    fn spawnBerserkerEnemy(self: *Self) Enemy {
+        var uniform_uint = zprob.UniformInt(u8).init(self.env.getRand());
+        const max_health = uniform_uint.sample(25, 30);
+        return Enemy{
+            .enemy_type = .Berserker,
+            .x = self.env.rUniformUInt(0, MAX_WIDTH - 1),
+            .y = self.env.rUniformUInt(0, MAX_HEIGHT - 1),
+            .max_health = max_health,
+            .current_health = max_health,
+            .attack = uniform_uint.sample(14, 18),
+            .defense = uniform_uint.sample(11, 15),
+        };
+    }
+};
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var gpa = std.heap.DebugAllocator(.{}){};
     const allocator = gpa.allocator();
     defer {
         const status = gpa.deinit();
@@ -101,7 +100,7 @@ pub fn main() !void {
         };
     }
 
-    var enemy_spawner = try EnemySpawner().init(allocator);
+    var enemy_spawner = try EnemySpawner.init(allocator);
     defer enemy_spawner.deinit();
 
     const enemies = try enemy_spawner.spawnEnemies(50);
