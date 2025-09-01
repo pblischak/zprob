b4450256de6502381795f496cf441f26eda21463
Paul Blischak <paul.blischak@gmail.com>
2025-09-01 15:43:02 -0400

    feat!: Updating re-exports and default distrs
    Distributions can now be imported using their default types
    (`default_*`). The distribution struct imports are unchanged, but must
    be initialized as empty structs (`{}`).
    
    BREAKING CHANGE: Imports remain largely the same but now default
    distribution types can be imported.

--- src/zprob.zig
+++ src/zprob.zig
@@ -16,6 +16,13 @@ pub const Poisson = @import("poisson.zig").Poisson;
 pub const UniformInt = @import("uniform.zig").UniformInt;
 pub const Weighted = @import("sample.zig").Weighted;
 
+pub const default_bernoulli = @import("bernoulli.zig").Bernoulli(u32, f64){};
+pub const default_binomial = @import("binomial.zig").Binomial(u32, f64){};
+pub const default_geometric = @import("geometric.zig").Geometric(u32, f64){};
+pub const default_negative_binomial = @import("negative_binomial.zig").NegativeBinomial(u32, f64){};
+pub const default_poisson = @import("poisson.zig").Poisson(u32, f64){};
+pub const default_uniform_int = @import("uniform.zig").UniformInt(i32){};
+
 // Continuous Probability Distributions
 pub const Beta = @import("beta.zig").Beta;
 pub const Cauchy = @import("cauchy.zig").Cauchy;
@@ -25,6 +32,14 @@ pub const Exponential = @import("exponential.zig").Exponential;
 pub const Gamma = @import("gamma.zig").Gamma;
 pub const Normal = @import("normal.zig").Normal;
 pub const Uniform = @import("uniform.zig").Uniform;
+
+pub const default_beta = @import("beta.zig").Beta(f64){};
+pub const default_cauchy = @import("cauchy.zig").Cauchy(f64){};
+pub const default_chi_squared = @import("chi_squared.zig").ChiSquared(u32, f64){};
+pub const default_exponential = @import("exponential.zig").Exponential(f64){};
+pub const default_gamma = @import("gamma.zig").Gamma(f64){};
+pub const default_normal = @import("normal.zig").Normal(f64){};
+pub const default_uniform = @import("uniform.zig").Uniform(f64){};
 
 pub const special_functions = @import("special_functions.zig");
 pub const utils = @import("utils.zig");
